ee7656d42914f8d3ac5c16fc25947605d80f843d
HenryLVT <80000500+HenryLVT@users.noreply.github.com>
2021-03-03 12:00:28 -0700

    Add files via upload

---
+++ Parsons.py
@@ -0,0 +1,23 @@
+import random
+y = 0
+x = random.randint(0,5)
+
+For_loop = ("10","20","30")
+
+def greater_then2():
+    y = 7
+    
+
+if x > 2:
+    greater_then2()
+    print(y)
+    print("ans, >2")
+
+else:
+    y = 9
+    for y in For_loop:
+        
+        print(For_loop)
+        print("ans =")
+        print("For Loop")
+        
