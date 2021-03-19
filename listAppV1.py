e38776b2ae94f5d92e57986e952daedba8c57d5f
HenryLVT <80000500+HenryLVT@users.noreply.github.com>
2021-03-19 11:48:38 -0600

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -9,7 +9,10 @@ def mainProgram():
             choice = input("""1. Add to list,
 2. Return the value of an index position
 3. Random Search
-4. End program
+4. Add a bunch of numbers
+5. Print contents of list
+6. Do a linear search
+7. End program
 """)
             if choice == "1":
                 addToList()
@@ -17,13 +20,27 @@ def mainProgram():
                 indexValues()
             elif choice == "3":
                 randomSearch()
+            elif choice == "4":
+                addABunch()
+            elif choice == "5":
+                print(myList)
+            elif choice == "6":
+                linearSearch()
+                
             else:
                 break
         except:
-            print("""hehe hoohoo that wasn't clever
-Restarting""")
+            print("""Hehe hoohoo that wasn't clever
+Restarting...""")
                 
-            
+def addABunch():
+        print("We're gonna add a bunch of numbers")
+        numToAdd = input("How many intgers do you want to add?   ")
+        numRange = input("And how high would you like them to go?")
+        for x in range (0,int(numToAdd)):
+            myList.append(random.randint(0, int(numRange)))
+        print("Your list is now complete")
+           
 def addToList():
     newItem = input("Please type an integer.")
     myList.append(int(newItem))
@@ -38,6 +55,18 @@ def randomSearch():
 def indexValues():
     indexPos = input("Where would you like to look")
     print(myList[int(indexPos)])
+
+def linearSearch():
+    print("We're going to search throught the list in the worst way possible")
+    searchItem = input("what are you looking for? Number wise")
+    amountOfTimes = 0
+    for x in range (len(myList)):
+        if myList[x] == int(searchItem):
+            amountOfTimes = amountOfTimes + 1
+            print("your item is at index {}".format(x))
+    print("Your item is in the list", amountOfTimes,"times")
+
+            
 
 if __name__ == "__main__":
     mainProgram()
